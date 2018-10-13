b3dd7dedcb71dcc3aa0c2a64bec8d29af75e76f8
Shreyas Patankar <ssp1996@gmail.com>
2018-10-13 09:52:54 +0530

    Update website list

--- WebsiteBlocker.py
+++ WebsiteBlocker.py
@@ -2,7 +2,7 @@ import time
 from datetime import datetime as dt
 hosts_path = "/etc/hosts"
 redirect = "127.0.0.1"
-website_list = ["www.facebook.com","facebook.com","www.youtube.com"]
+website_list = ["www.facebook.com","facebook.com","www.youtube.com", "www.gaana.com"]
 while True:
     if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,16):
         print("Working hours...!")
